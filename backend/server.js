68643a5c3a698acecc512c6d5fadd8c011864f8f
Your Name <jessica971231@yahoo.co.nz>
2023-03-30 08:07:08 +1300

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
-import express from "express"
-import cors from "cors"
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Backend for Coin is running on 3001');
+});
+
+// Start the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
