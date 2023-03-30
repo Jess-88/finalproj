f085727514579b21bcf48844e083dc63b6090fb4
Your Name <jessica971231@yahoo.co.nz>
2023-03-31 01:37:42 +1300

    finished code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,58 @@
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const db = require('./db');
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('Backend for Coin is running on 8080');
+// });
+
+// app.post('/milestones', async (req, res) => {
+//   const { occasion, date, amount } = req.body;
+//   console.log(req.body)
+//   for (const data of req.body) {
+//   try {
+//     await db.query(
+//       'INSERT INTO milestones (occasion, date, amount) VALUES ($1, $2, $3)',
+//       [data.occasion, data.date, data.amount]
+//     );
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).send('Server error');
+//   }
+//   }
+//   res.status(201).json({message: 'Milestone added successfully'});
+// });
+
+// app.get('/milestones', async (req, res) => {
+//   try {
+//     const result = await db.query('SELECT * FROM milestones ORDER BY date ASC');
+//     res.status(200).json(result.rows);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Server error');
+//   }
+// });
+
+// // Start the server
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// new code to clear history after restart
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require('./db');
 
 const app = express();
 
@@ -8,13 +60,54 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Function to reset the milestones table
+async function resetMilestonesTable() {
+  try {
+    await db.query('TRUNCATE TABLE milestones');
+    console.log('Successfully reset the milestones table');
+  } catch (error) {
+    console.error('Error resetting the milestones table:', error);
+  }
+}
+
+// Call the resetMilestonesTable function
+resetMilestonesTable();
+
 // Routes
 app.get('/', (req, res) => {
-  res.send('Backend for Coin is running on 3001');
-});
-
-// Start the server
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    res.send('Backend for Coin is running on 8080');
+  });
+  
+  app.post('/milestones', async (req, res) => {
+    const { occasion, date, amount } = req.body;
+    console.log(req.body)
+    for (const data of req.body) {
+      try {
+        await db.query(
+          'INSERT INTO milestones (occasion, date, amount) VALUES ($1, $2, $3)',
+          [data.occasion, data.date, data.amount]
+        );
+      } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server error');
+      }
+    }
+    res.status(201).json({message: 'Milestone added successfully'});
+  });
+  
+  app.get('/milestones', async (req, res) => {
+    try {
+      const result = await db.query('SELECT * FROM milestones ORDER BY date ASC');
+      res.status(200).json(result.rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+  });
+  
+  // Start the server
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
--- plan.js
+++ plan.js
@@ -1,54 +1,107 @@
-// Retrieve the milestones data from localStorage
-const milestonesData = localStorage.getItem("milestones");
-const milestones = JSON.parse(milestonesData);
+// // Retrieve the milestones data from localStorage
+// const milestonesData = localStorage.getItem("milestones");
+// const milestones = JSON.parse(milestonesData);
 
-// Sort milestones by date
-milestones.sort((a, b) => new Date(a.date) - new Date(b.date));
+async function fetchMilestones() {
+  try {
+    const response = await fetch("http://localhost:8080/milestones");
 
-// Function to calculate weeks between today and occasion date
-function weeksBetweenTodayAndOccasion(occasionDate) {
-  const today = new Date();
-  const oneWeekInMilliseconds = 1000 * 60 * 60 * 24 * 7;
-  const millisecondsBetweenDates = new Date(occasionDate) - today;
-  const weeks = millisecondsBetweenDates / oneWeekInMilliseconds;
-
-  return Math.ceil(weeks);
-}
-
-// Insert the milestones into the table
-const table = document.getElementById('savings-table');
-for (const milestone of milestones) {
-  const row = table.insertRow();
-  const occasionCell = row.insertCell();
-  const dateCell = row.insertCell();
-  const savingsCell = row.insertCell();
-  occasionCell.textContent = milestone.occasion;
-  dateCell.textContent = new Date(milestone.date).toDateString();
-  savingsCell.textContent = '';
-}
-
-// Calculate the savings per week for each occasion and update the table
-function calculateSavingsPerWeekAndUpdateTable() {
-  let savingsPerWeekArray = [];
-
-  for (let i = 0; i < milestones.length; i++) {
-    let savingsPerWeek = 0;
-    for (let j = i; j < milestones.length; j++) {
-      const weeksToMilestone = weeksBetweenTodayAndOccasion(milestones[j].date);
-      if (weeksToMilestone > 0) {
-        const currentMilestoneSavingsPerWeek = milestones[j].amount / weeksToMilestone;
-        savingsPerWeek += currentMilestoneSavingsPerWeek;
-      }
+    if (response.ok) {
+      const milestones = await response.json();
+      return milestones;
+    } else {
+      throw new Error("Failed to fetch milestones data");
     }
-    savingsPerWeekArray.push(savingsPerWeek);
-  }
-
-  // Update the savings per week column with calculated values
-  const savingsCells = table.querySelectorAll('td:nth-child(3)');
-  for (let i = 0; i < savingsCells.length; i++) {
-    const cell = savingsCells[i];
-    cell.textContent = '$' + savingsPerWeekArray[i].toFixed(2);
+  } catch (error) {
+    console.error("Error:", error);
+    alert("An error occurred while fetching milestones data. Please try again.");
+    return [];
   }
 }
 
-calculateSavingsPerWeekAndUpdateTable();
+async function init() {
+  const milestones = await fetchMilestones();
+
+  // Sort milestones by date
+  milestones.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  // Function to calculate weeks between today and occasion date
+  function weeksBetweenTodayAndOccasion(occasionDate) {
+    const today = new Date();
+    const oneWeekInMilliseconds = 1000 * 60 * 60 * 24 * 7;
+    const millisecondsBetweenDates = new Date(occasionDate) - today;
+    const weeks = millisecondsBetweenDates / oneWeekInMilliseconds;
+
+    return Math.ceil(weeks);
+  }
+
+  // Insert the milestones into the table
+  const table = document.getElementById('savings-table');
+  for (const milestone of milestones) {
+    const row = table.insertRow();
+    const occasionCell = row.insertCell();
+    const dateCell = row.insertCell();
+    const savingsCell = row.insertCell();
+    occasionCell.textContent = milestone.occasion;
+    dateCell.textContent = new Date(milestone.date).toDateString();
+    savingsCell.textContent = '';
+  }
+
+// // Calculate the savings per week for each occasion and update the table
+// function calculateSavingsPerWeekAndUpdateTable() {
+//   let savingsPerWeekArray = [];
+
+//   for (let i = 0; i < milestones.length; i++) {
+//     let savingsPerWeek = 0;
+//     for (let j = i; j < milestones.length; j++) {
+//       const weeksToMilestone = weeksBetweenTodayAndOccasion(milestones[j].date);
+//       if (weeksToMilestone > 0) {
+//         const currentMilestoneSavingsPerWeek = milestones[j].amount / weeksToMilestone;
+//         savingsPerWeek += currentMilestoneSavingsPerWeek;
+//       }
+//     }
+//     savingsPerWeekArray.push(savingsPerWeek);
+//   }
+
+//   // Update the savings per week column with calculated values
+//   const savingsCells = table.querySelectorAll('td:nth-child(3)');
+//   for (let i = 0; i < savingsCells.length; i++) {
+//     const cell = savingsCells[i];
+//     cell.textContent = '$' + savingsPerWeekArray[i].toFixed(2);
+//   }
+// }
+
+// calculateSavingsPerWeekAndUpdateTable();
+
+// }
+
+// init();
+
+  // Calculate the savings per week for each occasion and update the table
+  function calculateSavingsPerWeekAndUpdateTable() {
+    let savingsPerWeekArray = [];
+
+    for (let i = 0; i < milestones.length; i++) {
+      let savingsPerWeek = 0;
+      for (let j = i; j < milestones.length; j++) {
+        const weeksToMilestone = weeksBetweenTodayAndOccasion(milestones[j].date);
+        if (weeksToMilestone > 0) {
+          const currentMilestoneSavingsPerWeek = milestones[j].amount / weeksToMilestone;
+          savingsPerWeek += currentMilestoneSavingsPerWeek;
+        }
+      }
+      savingsPerWeekArray.push(savingsPerWeek);
+    }
+
+    // Update the savings per week column with calculated values
+    const savingsCells = table.querySelectorAll('td:nth-child(3)');
+    for (let i = 0; i < savingsCells.length; i++) {
+      const cell = savingsCells[i];
+      cell.textContent = '$' + savingsPerWeekArray[i].toFixed(2);
+    }
+  }
+
+  calculateSavingsPerWeekAndUpdateTable();
+}
+
+init();
